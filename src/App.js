b18bf406825692c54b8cef1b16e4a6b40d520bcf
Yakov Dev <ymilner@otn.ca>
2020-02-16 14:47:29 -0500

    L23 - show items as an h1 list

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: "This is string from state"
+      monsters: [
+        { name: "Frankenstein", id: "1" },
+        { name: "Dracula", id: "2" },
+        { name: "Zombie", id: "3" }
+      ]
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <p>
-            Edit <code>src/App.js</code> and save to reloadddddd YakovMM!!!.
-          </p>
-          <button onClick={() => this.setState({ string: "CHANGED!!" })}>
-            Change State Text
-          </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
