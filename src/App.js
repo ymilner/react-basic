a6e2e42c3008549e91b627b101ff9234d3cb5e1c
Yakov Dev <ymilner@otn.ca>
2020-02-16 15:15:12 -0500

    L26 - fetching the list of the users

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        { name: "Frankenstein", id: "1" },
-        { name: "Dracula", id: "2" },
-        { name: "Zombie", id: "3" }
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState((this.state.monsters = users)));
   }
 
   render() {
