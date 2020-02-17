2fecbec5d1a79e26516bbe510dbf9a2bc57bdb68
Yakov Dev <ymilner@otn.ca>
2020-02-16 21:09:23 -0500

    L29 - implemented CardList component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -10,18 +11,20 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <h1>Monsters Rolodex</h1>
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList>
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
