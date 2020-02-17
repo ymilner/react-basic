5a0228f33130c4462698d3ef6d66ab72ed70a734
Yakov Dev <ymilner@otn.ca>
2020-02-17 11:02:55 -0500

    L30 - implemented Card component

--- src/App.js
+++ src/App.js
@@ -20,11 +20,7 @@ class App extends Component {
     return (
       <div className='App'>
         <h1>Monsters Rolodex</h1>
-        <CardList>
-          {this.state.monsters.map(monster => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))}
-        </CardList>
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import './card-list.styles.css';
+import { Card } from '../card/card.component';
 
 export const CardList = props => {
-  console.log('card-list:props ', props);
-  return <div className='card-list'>{props.children}</div>;
+  // console.log('card-list:props ', props);
+  return (
+    <div className='card-list'>
+      {props.monsters.map(monster => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
 };
